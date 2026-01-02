5e24640cf58d70bf4b66cc9f4875d21f6b324fb2
Tal Partosh <partush2911@gmail.com>
2026-01-02 21:22:08 +0200

    working on mobile

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.github.doglover29.puppypad',
+  appName: 'PuppyHub',
+  webDir: 'dist'
+};
+
+export default config;
